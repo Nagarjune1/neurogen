4639450de7cf13ead448b41ffa944823e85ddb26
Tomek <tomaszwozniak96@gmail.com>
2018-06-28 20:02:28 +0200

    Initial commit

---
+++ src/pl/wozniaktomek/ThesisApp.java
@@ -0,0 +1,13 @@
+package pl.wozniaktomek;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class ThesisApp extends Application {
+
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+}
---
+++ src/pl/wozniaktomek/layout/Control/WindowControl.java
@@ -0,0 +1,14 @@
+package pl.wozniaktomek.layout.Control;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class WindowControl implements Initializable {
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
