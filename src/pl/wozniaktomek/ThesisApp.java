00719acfaef43ed47df600977fb86e4e792fb227
Tomek Woźniak <tomaszwozniak96@gmail.com>
2019-01-02 10:37:48 +0100

    added responsive resolution

--- src/pl/wozniaktomek/ThesisApp.java
+++ src/pl/wozniaktomek/ThesisApp.java
@@ -3,8 +3,10 @@ package pl.wozniaktomek;
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 import pl.wozniaktomek.layout.control.WindowControl;
 
@@ -32,7 +34,7 @@ public class ThesisApp extends Application {
     private void setStage(Stage primaryStage) {
         stage = primaryStage;
         stage.setTitle("NeuroGen");
-        stage.setScene(new Scene(root, 1280, 720));
+        setResolution(stage);
         stage.setMinWidth(1024);
         stage.setMinHeight(600);
 
@@ -40,5 +42,26 @@ public class ThesisApp extends Application {
             Platform.exit();
             System.exit(0);
         });
+    }
+
+    private void setResolution(Stage stage) {
+        Rectangle2D screenResolution = Screen.getPrimary().getVisualBounds();
+        System.out.println(screenResolution.getWidth() + " x " + screenResolution.getHeight());
+
+        double width, height;
+
+        if (screenResolution.getWidth() <= 1280) {
+            width = 1024;
+        } else {
+            width = 1280;
+        }
+
+        if (screenResolution.getHeight() <= 728) {
+            height = 628;
+        } else {
+            height = 720;
+        }
+
+        stage.setScene(new Scene(root, width, height));
     }
 }
